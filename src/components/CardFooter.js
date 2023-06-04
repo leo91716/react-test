11925832b7050f5a0b4c632d069d4252bea07330
leo <leo91716@gmail.com>
2023-06-04 22:11:42 +0800

    pass argument to children component

--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -1,3 +1,37 @@
-const CardFooter = () => <div className="card-footer">fast</div>
-
+const CardFooter = ({inputValue}) => {
+    let criteria={};
+    if (!inputValue){
+        criteria={
+            title:'---',
+            backgroundColor:'#d3d8e2'
+        }
+    }
+    else if(inputValue<15){
+        criteria={
+            title:'SLOW',
+            backgroundColor:'#ee362d'
+        }
+    }
+    else if(inputValue<40){
+        criteria={
+            title:'GOOD',
+            backgroundColor:'#1b82f1'
+        }
+    }
+    else{
+        criteria={
+            title:'FAST',
+            backgroundColor:'#13d569'
+        }
+    }
+    return (
+        <div className="card-footer"
+            style={{backgroundColor:criteria.backgroundColor
+            }}     
+        >
+            {criteria.title}
+        </div>
+    )
+    
+}
 export default CardFooter;
