be10db6369cf0225fb9e84163c7794eaed543779
陳軒竹LEOCHEN <leochen@mitac.com.tw>
2023-06-06 10:10:20 +0800

    add fontawesome

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import { useState } from 'react';
 import UnitControl from './components/UnitControl';
 import CardFooter from './components/CardFooter';
+import UnitConverter from './components/UnitConverter';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fab,fas,far);
+
 
 
 function App() {
@@ -15,23 +23,10 @@ function App() {
     <div className="card-header">Network Speed Converter</div>
     <div className="card-body">
       <UnitControl/>
-      <div className="converter">
-        <div className="flex-1">
-          <div className="converter-title">Set</div>
-          <input type="number"
-            onChange={handleInputChange}
-            value={inputValue}
-          className="input-number" min="0" />
-        </div>
-        <span className="angle-icon fa-2x" style={{marginTop: 30}}>
-          <i className="fas fa-angle-right" ></i>
-        </span>
-        <div className="text-right flex-1">
-          <div className="converter-title">Show</div>
-          <input type="text" className="input-number text-right" disabled
-           value={inputValue/8} />
-        </div>
-      </div>
+      <UnitConverter
+        inputValue={inputValue}
+        handleInputChange={handleInputChange}
+      />
     </div>
     <CardFooter inputValue={inputValue}/>
   </div>
